89fcbff58fc3ef11b0160837096361348556c483
Patrick Bailey <patrickbailey@Patricks-MacBook-Pro.local>
2018-04-22 00:12:21 -0700

    setup firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBFIFTGbwBCVMxpd74vgJnZ38OwRQDkPAU',
+      authDomain: 'authentication-816ab.firebaseapp.com',
+      databaseURL: 'https://authentication-816ab.firebaseio.com',
+      projectId: 'authentication-816ab',
+      storageBucket: 'authentication-816ab.appspot.com',
+      messagingSenderId: '252879785234',
+    });
+  }
   render() {
     return (
       <View>
