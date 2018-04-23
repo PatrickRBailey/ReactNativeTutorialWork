6f14b69723707a3ad91ca18417e5bb0cd523a14b
Patrick Bailey <patrick@ieqtechnology.com>
2018-04-23 16:55:31 -0700

    log out functionality

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyBFIFTGbwBCVMxpd74vgJnZ38OwRQDkPAU',
@@ -15,12 +17,36 @@ class App extends Component {
       storageBucket: 'authentication-816ab.appspot.com',
       messagingSenderId: '252879785234',
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
+  }
+
   render() {
     return (
-      <View>
+      <View style={{ height: 100 }}>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
